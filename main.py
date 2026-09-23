3342d052cc82bc29b5d6fd9cc8c03d2bb7b29c41
Jack Eddy <125694054+eddyjc89@users.noreply.github.com>
2026-09-23 15:29:13 -0400

    inital commit, functional main shell

---
+++ main.py
@@ -0,0 +1,18 @@
+# main.py
+
+#imports
+import numpy
+import pandas
+import matplotlib.pyplot
+from scipy.optimize import curve_fit
+from sklearn.linear_model import LinearRegression
+from sklearn.manifold import MDS
+from matplotlib.lines import Line2D
+import matplotlib.patches as mpatch
+
+# main
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
